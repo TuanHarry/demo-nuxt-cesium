1ddac22c8138eb73103c183b048b0b046d1a3654
Tran Tuan <tuantran.dev97@gmail.com>
2021-12-02 15:43:45 +0700

    Update box

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
 	<div class="w-full h-full relative">
 		<vc-viewer
 			:animation="animation"
-			:timeline="timeline"
 			:camera.sync="camera"
+			:fullscreenButton="true"
 			@ready="ready"
 		>
 			<!-- nav control  -->
@@ -39,6 +39,28 @@
 				:options="options"
 				:entities="entities"
 			></vc-datasource-geojson>
+
+			<vc-entity
+				:position="position1"
+				:point.sync="point1"
+			>
+				<vc-graphics-point
+					ref="point1"
+					:color="color1"
+					:pixelSize="8"
+				></vc-graphics-point>
+			</vc-entity>
+
+			<vc-collection-primitive :show="true">
+				<vc-primitive-model
+					ref="model"
+					:url="url"
+					:modelMatrix="modelMatrix"
+					:scale="10000"
+					:minimumPixelSize="128"
+					:maximumScale="200000"
+				></vc-primitive-model>
+			</vc-collection-primitive>
 		</vc-viewer>
 
 		<!-- option control  -->
@@ -65,6 +87,17 @@
 				></toggle-button>
 				<span class="ml-2 font-bold text-gray-600">Graphics</span>
 			</div>
+			<div class="flex flex-row items-center">
+				<toggle-button
+					ref="toggle_box"
+					:id="'box'"
+					:labelEnableText="''"
+					:labelDisableText="''"
+					:defaultState="showBox"
+					@change="change"
+				></toggle-button>
+				<span class="ml-2 font-bold text-gray-600">Box</span>
+			</div>
 		</div>
 
 	</div>
@@ -78,7 +111,6 @@ export default {
 	data() {
 		return {
 			animation: true,
-			timeline: true,
 			camera: {
 				position: {
 					lng: 106.6178,
@@ -99,35 +131,77 @@ export default {
 				enableMyLocation: true,
 				stroke: "red",
 			},
+			cesiumGlobal: null,
 
 			dataAreaVN: areaVN,
 			entities: [],
+
+			// Point
+			point1: null,
+			color1: {},
+			position1: { lng: 106.6178, lat: 10.8 },
+
+			// Model
+			modelMatrix: {},
+			url: "~/static/Duck.gltf",
+
 			// Control
 			showPolygon: false,
 			showGraphics: false,
+			showBox: true,
 		};
 	},
+
+	mounted() {
+		Promise.all([this.$refs.point1.createPromise]).then((instances) => {
+			instances[0].viewer.zoomTo(instances[0].viewer.entities);
+		});
+	},
+
 	methods: {
 		ready(cesiumInstance) {
 			const { Cesium, viewer } = cesiumInstance;
+			this.cesiumGlobal = cesiumInstance;
 			// Get Cesium and viewer instances here, then execute the relevant logic code
 			this.camera.position.lng = 106.6178;
 			this.camera.position.lat = 10.8;
 			this.camera.position.height = 5000;
 			this.animation = true;
 
-			// Get the array of entities
+			this.color1 = Cesium.Color.RED;
 
-			this.dataAreaVN.features.forEach((entity) => {
-				console.debug("entity", entity);
-				this.entities.push({
-					fill: entity.properties.fill_color,
-				});
-			});
+			// viewer.entities.add({
+			// 	name: "Blue box",
+			// 	position: Cesium.Cartesian3.fromDegrees(106.6178, 10.8, 0.0),
+			// 	box: {
+			// 		dimensions: new Cesium.Cartesian3(40.0, 30.0, 200.0),
+			// 		material: Cesium.Color.BLUE,
+			// 	},
+			// });
+
+			this.addBox();
+
+			this.modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
+				Cesium.Cartesian3.fromDegrees(106.6178, 10.8, 100)
+			);
 		},
 
 		subReady(cesiumInstance) {
 			cesiumInstance.viewer.zoomTo(cesiumInstance.cesiumObject);
+		},
+
+		addBox() {
+			console.debug(this.cesiumGlobal);
+			if (this.cesiumGlobal) {
+				this.cesiumGlobal.viewer.entities.add({
+					name: "Blue box",
+					position: Cesium.Cartesian3.fromDegrees(106.6178, 10.8, 0.0),
+					box: {
+						dimensions: new Cesium.Cartesian3(40.0, 30.0, 200.0),
+						material: Cesium.Color.BLUE,
+					},
+				});
+			}
 		},
 
 		// Action change polygon
@@ -137,6 +211,8 @@ export default {
 				this.showPolygon = val;
 			} else if ("polygon_graphics_button" === id) {
 				this.showGraphics = val;
+			} else if ("box_button" === id) {
+				this.showBox = val;
 			}
 		},
 	},
